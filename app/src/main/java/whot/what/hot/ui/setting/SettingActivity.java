1da1558db8a9689d9a1f27033ee7a11ed61576ab
Kevin <quickey123@gmail.com>
2018-02-14 14:34:06 +0800

    新增Google Map api

---
+++ app/src/main/java/whot/what/hot/ui/setting/SettingActivity.java
@@ -0,0 +1,13 @@
+package whot.what.hot.ui.setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
